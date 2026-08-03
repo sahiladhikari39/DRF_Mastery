dff9ca2f7d5766c698d3a8bb46ee7ba2e3e49dec
Sahil Adhikari <sahiladhi39@gmail.com>
2026-08-03 13:34:30 +0545

    created a model, serializer, views and urls for Student

---
+++ students/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import StudentListCreateView
+
+
+urlpatterns = [
+    path('students/', StudentListCreateView.as_view()),
+]
--- students/views.py
+++ students/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+from .models import Student
+from .serializers import StudentSerializer
+
 
 # Create your views here.
+
+class StudentListCreateView(APIView):
+    def get(self, request):
+        students = Student.objects.all()
+        serializer = StudentSerializer(students, many=True)
+        return Response(serializer.data)
+
+
+    def post(self, request):
+        serializer = StudentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
